739140f94d89c69d2a8713cefb54ea6752f85e2f
ogawa <ogawa@tagbangers.co.jp>
2014-04-28 09:23:43 +0900

    パフォーマンス改善

--- src/main/java/org/wallride/core/service/ArticleService.java
+++ src/main/java/org/wallride/core/service/ArticleService.java
@@ -359,7 +359,7 @@ public class ArticleService {
 		term.getCategoryCodes().add(code);
 		term.setStatus(status);
 
-		Pageable pageable = new PageRequest(1, size);
+		Pageable pageable = new PageRequest(0, size);
 		Page<Article> page = articleRepository.findByFullTextSearchTerm(term, pageable);
 		return new TreeSet<>(page.getContent());
 	}
