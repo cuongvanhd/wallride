bb0e6e36194f60024ee3537e241c1b229ae196f5
Nguyen Van Cuong <cuong@tagbangers.co.jp>
2015-10-06 12:01:33 +0900

    wallrideプロジェクトのテストを作成。 PaginationTestクラスのソースコードを修正。ソースコードができました。

--- src/test/java/org/wallride/core/support/PaginationTest.java
+++ src/test/java/org/wallride/core/support/PaginationTest.java
@@ -1,5 +1,7 @@
 package org.wallride.core.support;
 
+import java.util.List;
+
 import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,8 +13,6 @@ import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-
-import java.util.List;
 
 import static org.mockito.Mockito.*;
 
